824070a033a674b14e01ed55884820af1231424a
ModiHitiksha <hitiksha.200410107112@gmail.com>
2024-04-30 16:48:20 +0530

    commit

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection con = new SqlConnection("Data Source =LAPTOP-45N6M36C\\SQLEXPRESS; database = practice; Integrated Security=SSPI;");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = " insert into temp1 values('TextBox1','TextBox2')";
+            con.Open();
+
+            SqlDataReader dr1 = cmd.ExecuteReader();
+            dr1.Read();
+        
+        }
+    }
+}
