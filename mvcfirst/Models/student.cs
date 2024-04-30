ae6798b03010cf15958efa9b9d0b74973b9a1cfc
ModiHitiksha <hitiksha.200410107112@gmail.com>
2024-04-30 16:49:28 +0530

    commit

---
+++ mvcfirst/Models/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvcfirst.Models
+{
+    public class student
+    {
+            public int StudentId { get; set; }
+            public string StudentName { get; set; }
+            public int Age { get; set; }
+        
+    }
+}
